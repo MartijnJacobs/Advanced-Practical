195b9812b5d91ebf49e135d474a5f89fd24a3846
MartijnJacobs <59930170+MartijnJacobs@users.noreply.github.com>
2020-06-09 14:18:51 +0200

    Add files via upload

---
+++ rnd.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+###########################################################
+### Imports
+import numpy as np
+
+###########################################################
+### (vX, dL, iS)= Power(mA, dEpsilon)
+#def Power(mA, dEpsilon):
+class Patient(object):
+    def
+
+###########################################################
+### dL = EigenvalueRQ(mA, vX)   
+
+
+###########################################################
+### main
+def main():
+    # Magic numbers
+
+    # Initialisation
+    answer = 
+    # Estimation
+    # Output
+    print("answer:",answer)
+###########################################################
+### start main
+if __name__ == "__main__":
+    main()
